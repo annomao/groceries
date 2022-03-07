0cb6ef31944bb8c25a79efae504a8b1b2ac9c60d
annomao <omaoannkerubo@gmail.com>
2022-03-08 01:11:19 +0300

    add comments

--- js/script.js
+++ js/script.js
@@ -1,11 +1,18 @@
+//get button element
 let btn = document.getElementById("enter");
 let btnReset = document.getElementById("reset-btn")
- 
+
 function displayGroceries(){
   const items = document.getElementById("item").value;
-  itemSplits = items.split(",");
-  groceries = itemSplits.map(item => item.trim())
+  itemSplits = items.split(","); //splits input
+
+  //remove white space
+  groceries = itemSplits.map(item => item.trim());
+
+  //sort list
   groceries = groceries.sort();
+
+  //create list item
   let list = document.getElementById("item-list");
   for (let i = 0; i < groceries.length; i++) {
     let li = document.createElement("li");
@@ -13,6 +20,7 @@ function displayGroceries(){
     list.appendChild(li);
   }
 }
+//checks to see if list has items and deletes item if true
 function removeList() {
   const list = document.getElementById("item-list");
   while (list.hasChildNodes()) {
