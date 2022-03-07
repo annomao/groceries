9d1c6a48761d347c9ea23d08144f7c8d4f5575f3
annomao <omaoannkerubo@gmail.com>
2022-03-08 00:58:28 +0300

    add functionality to display list on webpage

--- js/script.js
+++ js/script.js
@@ -5,12 +5,15 @@ function displayGroceries(){
   itemSplits = items.split(",");
   groceries = itemSplits.map(item => item.trim())
   groceries = groceries.sort();
+  let list = document.getElementById("item-list");
   for (let i = 0; i < groceries.length; i++) {
-    console.log(groceries[i]); 
+    let li = document.createElement("li");
+    li.innerText = groceries[i];
+    list.appendChild(li);
   }
 }
 
 btn.addEventListener('click' , displayGroceries);
 btn.addEventListener("click", function(event){
   event.preventDefault()
-})
+ })
